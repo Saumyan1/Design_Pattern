cffe8e3026d60b6c51c4911f5c868711d270abfe
Saumyan Kanoo <saumyankanoo66@gmail.com>
2026-06-03 13:34:13 +0530

    added example 2 for design pattern in golang

--- strategy_patterns/example2/main.go
+++ strategy_patterns/example2/main.go
@@ -2,9 +2,71 @@ package main
 
 import "fmt"
 
+//Notification system
 
-func main(){
-	fmt.Println("Example 2")
+
+
+//strategy interface
+type Notification interface{
+	Send(message string)
 }
 
 
+//Concrete strategy
+
+
+//email-notification
+type EmailNotification struct{
+}
+func(e *EmailNotification) Send(message string){
+	fmt.Println("Sending Email -> ", message)	
+}
+
+//sms notification
+type SMSNotification struct{
+}
+func(s *SMSNotification) Send(message string){
+	fmt.Println("Sending sms -> ", message)
+}
+
+//push notification
+type PushNotification struct{
+}
+func(p *PushNotification) Send(message string){
+	fmt.Println("Sending pushNotification -> ", message)
+}
+
+//Context - NotificationServices
+
+type NotificationService struct{
+	Notif Notification
+}
+
+//constructor
+
+func NewNotificationService(n Notification) *NotificationService{
+	return &NotificationService{
+		Notif: n,	
+	}
+
+}
+
+func(ns *NotificationService) SetNotification(n Notification){
+	ns.Notif = n
+}
+
+func (ns *NotificationService) Notify(message string){
+	 ns.Notif.Send(message)
+}
+
+//CLIENT 
+
+func main(){
+	email := &EmailNotification{}
+	sm := &SMSNotification{}
+
+	service := NewNotificationService(email)
+	service.Notify("Order Placed")
+	service.SetNotification(sm)
+	service.Notify("Order Placed")
+}
